d9e2876b398c1a2544641d7ae3a27bd7f5d73d6f
Rayfess <153087368+Rayfess@users.noreply.github.com>
2026-06-24 21:51:00 +0700

    feat: adding init hangman

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { useState } from "react"
-import  words  from "./wordlists.json";
+import { useState } from "react";
+import words from "./wordlists.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 function App() {
   const [wordToGuess, setwordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
-  const [guessedLetts, setguessedLetts] = useState<string[]>([])
-  console.log(wordToGuess)
-  return <h1>hello world</h1>
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetts, setguessedLetts] = useState<string[]>([]);
+  console.log(wordToGuess);
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
