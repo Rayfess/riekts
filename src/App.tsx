3eb75da4e50e20345f853bf279f744fff40fa13a
Rayfess <153087368+Rayfess@users.noreply.github.com>
2026-06-24 17:34:56 +0700

    feat: planning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
+import  words  from "./wordlists.json";
 function App() {
+  const [wordToGuess, setwordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetts, setguessedLetts] = useState<string[]>([])
+  console.log(wordToGuess)
   return <h1>hello world</h1>
 }
 
