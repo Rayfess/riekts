713663ab5b185237335920688bcb2dc42629e3b1
Rayfess <153087368+Rayfess@users.noreply.github.com>
2026-06-25 09:44:43 +0700

    feat: add implement of keyboard input to match wordtoguess

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import words from "./wordlists.json";
 import { HangmanDrawing } from "./HangmanDrawing";
 import { HangmanWord } from "./HangmanWord";
 import { Keyboard } from "./Keyboard";
 function App() {
   const [wordToGuess, setwordToGuess] = useState(() => {
-    return "test";
-    // return words[Math.floor(Math.random() * words.length)];
+    return words[Math.floor(Math.random() * words.length)];
   });
 
-  const [guessedLetts, setguessedLetts] = useState<string[]>(["g", "t", "v"]);
+  const [guessedLetts, setguessedLetts] = useState<string[]>([]);
+
   const incorrLetts = guessedLetts.filter(
     (lett) => !wordToGuess.includes(lett),
   );
+
+  const addGuessedLett = (lett: string) => {
+    if (guessedLetts.includes(lett)) return;
+
+    setguessedLetts((currentLett) => [...currentLett, lett]);
+  };
+
+  console.log(wordToGuess);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLett(key);
+    };
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, []);
 
   return (
     <div
@@ -27,7 +51,7 @@ function App() {
     >
       <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
       <HangmanDrawing numOfGuesses={incorrLetts.length} />
-      <HangmanWord />
+      <HangmanWord guessedLetts={guessedLetts} wordToGuess={wordToGuess} />
       <div style={{ alignSelf: "stretch" }}>
         <Keyboard />
       </div>
